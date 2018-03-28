5b5e687e0264f4b9ec749ff6e2b4e465fd9b51b4
FerminT <FerminT@users.noreply.github.com>
2018-03-28 15:45:30 -0300

    Codigo funcionando
    Sigo sin entender por qué hay que cerrar los pipes

--- Taller IPC/tarea-taller/codigo/mini-shell.c
+++ Taller IPC/tarea-taller/codigo/mini-shell.c
@@ -8,7 +8,7 @@
 static int
 run(const char ***progs, size_t count)
 {
-	int pipes[4];
+	int pipes[2][2];
 	pid_t *children;
 	size_t i, j;
 	pid_t cur;
@@ -19,25 +19,18 @@ run(const char ***progs, size_t count)
 		exit(1);
 	}
 
-	//if (!(pipes = malloc(2 * sizeof(int) * count))){
-	//	fputs("out of memory\n", stderr);
-	//	exit(1);
-	//};
-
-	//printf("Count: %d\n", count);
-	for(int i = 0;i < 2*(count-1);i += 2)
-	{
-		if (pipe(&pipes[i]) == -1){
-			perror("pipes");
-			exit(1);
-		};
-		printf("Pipes: %d:%d\n", pipes[i], pipes[i+1]);
-	}
-
 	// TODO: crear pipes ANTES de crear los procesos
 	// Pensar cuantos pipes necesito.
+	if (pipe(pipes[0]) == -1) {
+		perror("pipe");
+		exit(1);
+	}
+	if (pipe(pipes[1]) == -1) {
+		perror("pipe");
+		exit(1);
+	}
 
-	for (i = 0; i < count; i++) {
+	for (int i = 0; i < count; i++) {
 
 		//TODO: Crea *count* procesos
 		cur = fork();
@@ -45,36 +38,53 @@ run(const char ***progs, size_t count)
 			perror("fork");
 			exit(1);
 		}
+
 		//TODO: Guardar el PID del proceso hijo en children[i]
-		if (cur != 0){
+		if (cur != 0)
 			children[i] = cur;
-			close(pipes[i]);
-			close(pipes[i+1]);
-		}
+
 		//TODO: Para cada proceso hijo:
 			//1. Redireccionar los file descriptors adecuados al proceso
 			//2- Ejecutar el programa correspondiente
 		if (cur == 0){
-
-			if (dup2(pipes[i], 0) == -1){
-				printf("%d\n", pipes[i]);
-				perror("dup2");
+			if (i != 0){ // redirijo el stdin de wc y awk
+				if (dup2(pipes[i - 1][0], 0) == -1){
+					perror("dup2");
+					exit(1);
+				}
+			}
+			if (i != 2){ // redirijo el stdout de ls y wc; awk lo tiene que escribir por pantalla
+				if (dup2(pipes[i][1], 1) == -1){
+					perror("dup2");
+					exit(1);
+				}
+			}
+			//Cierro exactamente los pipes que no uso
+			if (i == 0){
+				close(pipes[0][0]);
+				close(pipes[1][0]);
+				close(pipes[1][1]);
+			}
+			if (i == 1){
+				close(pipes[0][1]);
+				close(pipes[1][0]);
+			}
+			if (i == 2){
+				close(pipes[0][0]);
+				close(pipes[0][1]);
+				close(pipes[1][1]);
+			}
+			if (execvp(progs[i][0], (char * const*) progs[i]) == -1){
+				perror("execvp");
 				exit(1);
-			};
-
-			if (dup2(pipes[i+1], 1) == -1){
-				printf("%d\n", pipes[i + 1]);
-				perror("dup2");
-				exit(1);
-			};
-
-			//printf("%s\n", progs[i][0]);
-			if (execvp(progs[i][0], (char * const*)progs[i]) == -1){
-				perror("execv");
-				exit(1);
-			};
+			}
 		}
 	}
+	//Si no cerraba los pipes en el proceso padre, se quedaba esperando
+	close(pipes[0][0]);
+	close(pipes[0][1]);
+	close(pipes[1][0]);
+	close(pipes[1][1]);
 
 	//El padre espera a que terminen todos los procesos hijos que ejecutan los programas
 	for (i = 0; i < count; i++) {
@@ -105,7 +115,7 @@ main(int argc, char **argv)
 	char *awkcmd[] = { "awk", "{ print $2 }", NULL };
 	char **progs[] = { lscmd, wccmd, awkcmd };
 
-	printf("status: %d\n", run((const char ***)progs, NELEMS(progs)));
+	printf("status: %d\n", run((const char ***) progs, NELEMS(progs)));
 	fflush(stdout);
 	fflush(stderr);
 
