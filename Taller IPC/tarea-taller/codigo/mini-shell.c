e43c833c5b11ffcd3f5ac9dabd4d7ca94115e9c8
Emiliano <eslucero2010@gmail.com>
2018-03-29 11:17:13 -0300

    Emprolije lo que hizo fermin en mini-shell, saque ejecutables del repo

--- Taller IPC/tarea-taller/codigo/mini-shell.c
+++ Taller IPC/tarea-taller/codigo/mini-shell.c
@@ -8,7 +8,8 @@
 static int
 run(const char ***progs, size_t count)
 {
-	int pipes[2][2];
+        int cant_pipes = count -1;
+        int pipes[cant_pipes][2]; // Se puede hacer esto porque usamos C99
 	pid_t *children;
 	size_t i, j;
 	pid_t cur;
@@ -21,14 +22,12 @@ run(const char ***progs, size_t count)
 
 	// TODO: crear pipes ANTES de crear los procesos
 	// Pensar cuantos pipes necesito.
-	if (pipe(pipes[0]) == -1) {
-		perror("pipe");
-		exit(1);
-	}
-	if (pipe(pipes[1]) == -1) {
-		perror("pipe");
-		exit(1);
-	}
+        for(int i = 0; i < cant_pipes; i++){
+            if (pipe(pipes[i]) == -1) {
+                perror("pipe");
+                exit(1);
+            }           
+        }
 
 	for (int i = 0; i < count; i++) {
 
@@ -41,19 +40,23 @@ run(const char ***progs, size_t count)
 
 		//TODO: Guardar el PID del proceso hijo en children[i]
 		if (cur != 0)
-			children[i] = cur;
+		    children[i] = cur;
 
 		//TODO: Para cada proceso hijo:
 			//1. Redireccionar los file descriptors adecuados al proceso
 			//2- Ejecutar el programa correspondiente
 		if (cur == 0){
-			if (i != 0){ // redirijo el stdin de wc y awk
+			if (i != 0){ 
+                            // redirijo el stdin de wc y awk
+                            // Como no es el primer programa, su pipe de entrada toma de stdin
 				if (dup2(pipes[i - 1][0], 0) == -1){
 					perror("dup2");
 					exit(1);
 				}
 			}
-			if (i != 2){ // redirijo el stdout de ls y wc; awk lo tiene que escribir por pantalla
+			if (i != (count-1)){ 
+                            // redirijo el stdout de ls y wc; awk lo tiene que escribir por pantalla
+                            // Como no es el ultimo programa, su pipe de salida escribe en stdout
 				if (dup2(pipes[i][1], 1) == -1){
 					perror("dup2");
 					exit(1);
@@ -61,19 +64,34 @@ run(const char ***progs, size_t count)
 			}
 			//Cierro exactamente los pipes que no uso
 			if (i == 0){
-				close(pipes[0][0]);
-				close(pipes[1][0]);
-				close(pipes[1][1]);
+                            // Como es el primer programa, solo usa su pipe de escritura, cerramos todos los demas
+			    close(pipes[0][0]);
+                            for(int j = 1; j < cant_pipes;j++)
+                            {
+                                close(pipes[j][0]);
+                                close(pipes[j][1]);
+                            }
+			}else if (i == (count - 1)){
+                            // Como es el ultimo programa, solo usa su pipe de lectura, cerramos todos los demas
+			    close(pipes[i-1][1]);
+                            for(int j = 0; j < (cant_pipes-1);j++)
+                            {
+                                close(pipes[j][0]);
+                                close(pipes[j][1]);
+                            }
 			}
-			if (i == 1){
-				close(pipes[0][1]);
-				close(pipes[1][0]);
+                        else{
+                            // Es uno de los programas del medio
+                            // Usa pipes[i][1] y pipes[i-1][0], cerramos el resto
+                            for(int j = 0; j < cant_pipes;j++)
+                            {
+                                if (j != i)
+                                    close(pipes[j][1]);
+                                if (j != i-1)
+                                    close(pipes[j][0]);
+                            }
 			}
-			if (i == 2){
-				close(pipes[0][0]);
-				close(pipes[0][1]);
-				close(pipes[1][1]);
-			}
+			
 			if (execvp(progs[i][0], (char * const*) progs[i]) == -1){
 				perror("execvp");
 				exit(1);
@@ -81,10 +99,11 @@ run(const char ***progs, size_t count)
 		}
 	}
 	//Si no cerraba los pipes en el proceso padre, se quedaba esperando
-	close(pipes[0][0]);
-	close(pipes[0][1]);
-	close(pipes[1][0]);
-	close(pipes[1][1]);
+        for(int i = 0; i < cant_pipes; i++)
+        {
+            close(pipes[i][0]);
+            close(pipes[i][1]);
+        }
 
 	//El padre espera a que terminen todos los procesos hijos que ejecutan los programas
 	for (i = 0; i < count; i++) {
