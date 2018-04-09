d1f57db141557b3bc6652866b8f0879ee54d8be0
FerminT <fermintravi@gmail.com>
2018-04-09 16:42:41 -0300

    Clase hashmap un poco mas completa

--- TP1/entregable/ConcurrentHashMap.hpp
+++ TP1/entregable/ConcurrentHashMap.hpp
@@ -1,47 +1,70 @@
 #include <string>
 #include <list>
+#include <pthread.h>
+#include <stdio.h>
+#include <atomic>
+#include "ListaAtomica.hpp"
 
-class ConcurretHashMap{
-    ConcurrentHashMap();
-    ~ConcurrentHashMap();
+using namespace std;
 
-    void addAndInc(std::string key);
-    bool member(std::string key);
-    std::pair<std::string, unsigned int> maximum(unsigned int nt);
+class ConcurrentHashMap{
+	private:
+		int hash_key(string key);
+
+		// Acá probablemente tengamos que definir un array[26] de semaforos (inicializados en 1)
+		// No entiendo por qué especifican que addAndInc y maximum no pueden ser concurrentes,
+		// si nunca hay necesidad de ejecutarlos simultáneamente.
+	public:
+		// La hago publica porque los tests acceden directamente a ella
+		Lista<pair<string, unsigned int> >* tabla[26];
+
+	    ConcurrentHashMap();
+	    ~ConcurrentHashMap();
+
+	    void addAndInc(string key);
+	    bool member(string key);
+	    pair<string, unsigned int> maximum(unsigned int nt);
 };
 
+int ConcurrentHashMap::hash_key(string key){
+	int res = key.at(0) - 97;
+	return res;
+}
+
 ConcurrentHashMap::ConcurrentHashMap(){
-  //TODO
+	for (int i = 0; i < 26; i++)
+		tabla[i] = new Lista<pair<string, unsigned int> >();
 }
 
 ConcurrentHashMap::~ConcurrentHashMap(){
+	for (int i = 0; i < 26; i++)
+  		delete tabla[i];
+}
+
+void ConcurrentHashMap::addAndInc(string key){
   //TODO
 }
 
-void ConcurrentHashMap::addAndInc(std::string key){
+bool ConcurrentHashMap::member(string key){
   //TODO
 }
 
-bool ConcurrentHashMap::member(std::string key){
+pair<string, unsigned int> ConcurrentHashMap::maximum(unsigned int nt){
   //TODO
 }
 
-std::pair<std::string, unsigned int> ConcurrentHashMap::maximum(unsigned int nt){
+ConcurrentHashMap count_words(string arch){
   //TODO
 }
 
-ConcurrentHashMap count_words(std::string arch){
+ConcurrentHashMap count_words(list<string> archs){
   //TODO
 }
 
-ConcurrentHashMap count_words(std::list<std::string> archs){
+ConcurrentHashMap count_words(unsigned int n, list<string> args){
   //TODO
 }
 
-ConcurrentHashMap count_words(unsigned int n, std::list<std::string> args){
-  //TODO
-}
-
-std::pair<std::string, unsigned int> maximum(unsigned int p_archivos, unsigned int p_maximos, std::list<std::string> archs){
+pair<string, unsigned int> maximum(unsigned int p_archivos, unsigned int p_maximos, list<string> archs){
   //TODO
 }
