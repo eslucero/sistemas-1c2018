88b8d56ca491f461f5bde14f42a950d9901ef3bf
FerminT <fermintravi@gmail.com>
2018-04-11 18:10:49 -0300

    addAndInc e intento de maximum

--- TP1/entregable/ConcurrentHashMap.hpp
+++ TP1/entregable/ConcurrentHashMap.hpp
@@ -2,6 +2,7 @@
 #include <list>
 #include <pthread.h>
 #include <stdio.h>
+#include <semaphore.h>
 #include <atomic>
 #include "ListaAtomica.hpp"
 
@@ -10,10 +11,11 @@ using namespace std;
 class ConcurrentHashMap{
 	private:
 		int hash_key(string key);
+		void *buscar_maximo(void *p_nt);
 
-		// Acá probablemente tengamos que definir un array[26] de semaforos (inicializados en 1)
-		// No entiendo por qué especifican que addAndInc y maximum no pueden ser concurrentes,
-		// si nunca hay necesidad de ejecutarlos simultáneamente.
+		sem_t semaforo[26];
+		atomic_bool lock;
+		pair<string, unsigned int> max;
 	public:
 		// La hago publica porque los tests acceden directamente a ella
 		Lista<pair<string, unsigned int> >* tabla[26];
@@ -32,25 +34,98 @@ int ConcurrentHashMap::hash_key(string key){
 }
 
 ConcurrentHashMap::ConcurrentHashMap(){
-	for (int i = 0; i < 26; i++)
+	for (int i = 0; i < 26; i++){
 		tabla[i] = new Lista<pair<string, unsigned int> >();
+		sem_init(&semaforo[i], 0, 1);
+	}
+	max = make_pair("", 0);
 }
 
 ConcurrentHashMap::~ConcurrentHashMap(){
-	for (int i = 0; i < 26; i++)
+	for (int i = 0; i < 26; i++){
   		delete tabla[i];
+  		sem_destroy(&semaforo[i]);
+	}
 }
 
 void ConcurrentHashMap::addAndInc(string key){
-  //TODO
+	//sem_t mutex; mutex.wait() para que no sea concurrente con maximum
+	int k = hash_key(key);
+	// Obtengo acceso exclusivo de la lista a modificar
+	sem_wait(&semaforo[k]);
+	
+	bool no_esta = true;
+	for (auto it = tabla[k]->CrearIt(); it.HaySiguiente(); it.Avanzar()){
+		auto& t = it.Siguiente();
+		if (t.first == key){
+			t.second++;
+			no_esta = false;
+		}
+	}
+	if (no_esta){
+		pair<string, unsigned int> palabra(key, 1);
+		tabla[k]->push_front(palabra);
+		// Incrementar variable global con cant. de palabras?
+	}
+
+	sem_post(&semaforo[k]);
+	//mutex.signal()
 }
 
 bool ConcurrentHashMap::member(string key){
-  //TODO
+	bool esta = false;
+	int k = hash_key(key);
+	for (auto it = tabla[k]->CrearIt(); it.HaySiguiente(); it.Avanzar()){
+		auto t = it.Siguiente();
+		if (t.first == key)
+			esta = true;
+	}
+
+	return esta;
+}
+
+void *ConcurrentHashMap::buscar_maximo(void *p_nt){
+	pair<unsigned int, unsigned int> tid_nt = *((pair<unsigned int, unsigned int> *) p_nt);
+
+	for (unsigned int i = tid_nt.first; i < 26; i += tid_nt.second){
+		pair<string, unsigned int> max_fila("", 0);
+		for (auto it = tabla[i]->CrearIt(); it.HaySiguiente(); it.Avanzar()){
+			auto t = it.Siguiente();
+			if (t.second > max_fila.second)
+				max_fila = t;
+		}
+		// Hago un TTAS Spinlock
+		do{
+			while(lock.load()) continue;
+		}
+		while(lock.exchange(true));
+		// Tengo acceso exclusivo
+		if (max_fila.second > max.second)
+			max = max_fila;
+		lock.store(false);
+	}
+
+	return NULL;
 }
 
 pair<string, unsigned int> ConcurrentHashMap::maximum(unsigned int nt){
-  //TODO
+	// sem_t mutex; mutex.wait() para la no-concurrencia con addAndInc
+	pthread_t thread[nt];
+    unsigned int tid;
+    //A cada thread le paso su tid y la cant. de threads, para saber qué filas procesar
+    pair<unsigned int, unsigned int> tids[nt];
+    lock.store(false);
+
+    for (tid = 0; tid < nt; ++tid) {
+		tids[tid].first = tid;
+		tids[tid].second = nt;
+		pthread_create(&thread[tid], NULL, buscar_maximo, &tids[tid]);
+    }
+
+    for (tid = 0; tid < nt; ++tid)
+        pthread_join(thread[tid], NULL);
+
+    return max;
 }
 
 ConcurrentHashMap count_words(string arch){
